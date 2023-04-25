c1975d4a5d9ce508d625b95acf055c9c5a1d869e
miguel8888888 <86738300+miguel8888888@users.noreply.github.com>
2023-04-24 22:40:49 -0500

    vue

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <BabylonExamples/>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
+<script lang="ts">
+import { defineComponent } from 'vue';
+import BabylonExamples from './components/BabylonExamples.vue';
 
-export default {
+export default  defineComponent({
   name: 'App',
   components: {
-    HelloWorld
+    BabylonExamples
   }
-}
+});
 </script>
 
 <style>
